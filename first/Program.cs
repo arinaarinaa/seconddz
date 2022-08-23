510755ea53c82f43bdafae602b74e37515b89588
arinach <arina.tchukurova@yandex.ru>
2022-08-23 21:48:20 +0300

    add newprogram

--- first/Program.cs
+++ first/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введи тут трехзначное число - ");
+int x;
+x = Convert.ToInt32(Console.ReadLine());
+string numbers = Convert.ToString(x);
+Console.WriteLine(numbers[1]);
+
+
+
+
